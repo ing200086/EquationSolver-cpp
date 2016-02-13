227924a076dddc98f7d37251d4a1cd654ef81491
ing200086 <ing200086@gmail.com>
2016-02-12 19:05:05 -0600

    Moved parameterized values to header file

--- test/Unit/Token/Lexical/Number/NumberTest.cpp
+++ test/Unit/Token/Lexical/Number/NumberTest.cpp
@@ -6,9 +6,8 @@ TEST_P(TestValidIntegers, AreAccepted) {
 	ASSERT_TRUE(_numberToken->accepts(GetParam()));
 }
 
-INSTANTIATE_TEST_CASE_P(ValidIntegers, TestValidIntegers, Values(
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    ));
+INSTANTIATE_TEST_CASE_P(ValidIntegers, TestValidIntegers,
+	ValuesIn(_integers));
 
 class TestNonIntegers: public NumberTester {  };
 
@@ -16,11 +15,8 @@ TEST_P(TestNonIntegers, AreAccepted) {
 	ASSERT_FALSE(_numberToken->accepts(GetParam()));
 }
 
-INSTANTIATE_TEST_CASE_P(Alphas, TestNonIntegers, Values(
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-    ));
+INSTANTIATE_TEST_CASE_P(Alphas, TestNonIntegers,
+	ValuesIn(_alphas));
 
-INSTANTIATE_TEST_CASE_P(Symbols, TestNonIntegers, Values(
-    '+', '-', '/', '*', '='
-    ));
+INSTANTIATE_TEST_CASE_P(Symbols, TestNonIntegers,
+	ValuesIn(_symbols));
--- test/Unit/Token/Lexical/Number/NumberTest.h
+++ test/Unit/Token/Lexical/Number/NumberTest.h
@@ -4,7 +4,7 @@
 #include "gmock/gmock.h"
 	using ::testing::Test;
 	using ::testing::TestWithParam;
-	using ::testing::Values;
+	using ::testing::ValuesIn;
 
 #include <memory>
 	using ::std::unique_ptr;
@@ -17,3 +17,9 @@ protected:
 		_numberToken.reset(new Number());
 	}
 };
+
+const char _integers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+const char _alphas[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+						'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+						's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+const char _symbols[] = {'+', '-', '/', '*', '='};
